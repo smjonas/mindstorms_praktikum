a18db402768af4aff9751414f3b7c0c093b37789
lukas <luki.baldner@gmail.com>
2022-11-28 11:12:50 +0100

    Remove magic numbers, create states for obstacle, change drive parameters, fix state

--- src/robot.py
+++ src/robot.py
@@ -5,13 +5,13 @@ from pybricks.parameters import Button, Color, Direction, Port, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 
-from state import s
+from state import State
 
 ev3 = EV3Brick()
 
 
 class Robot:
-    DRIVE_SPEED = 50
+    
 
     def __init__(self, course):
         self.course = course
@@ -54,15 +54,19 @@ class Robot:
 
     # Stage 1: Following a white line with gaps and an obstacle
     def lines(self):
-        LIGHT, DARK, K = 66, 18, 0.5
+        LIGHT, DARK, K = 60, 18, 0.35
         mid = (LIGHT + DARK) / 2
 
-        # depends on search_angle
+        TURN_RATE = 10
+        DRIVE_SPEED = 70
+        WAIT_TIME = 0
+        TURNBACK_RATE = 30
+
         GAP_THRESHOLD_ANGLE = 84
         GAP_TURNBACK_ANGLE = 120
         OBSTACLE_DISTANCE = 100
 
-        NEXT_LEVEL_STATE, OBSTACLE_STATE = 1, 2
+        NEXT_LEVEL_STATE, OBSTACLE_STATE = "next_level", "obst_1"
 
         def is_dark():
             return self.color_sensor.reflection() <= DARK
@@ -80,19 +84,20 @@ class Robot:
         def turn(angle):
             def turn_angle():
                 self.robot.drive(0, angle)
-                wait(10)
+                wait(WAIT_TIME)
             return turn_angle
 
         # Lots of currying :D
         def did_turn(degrees):
             def did_turn_degrees():
-                delta = self.robot.angle() - self.prev_state.angle
+                delta = self.robot.angle() - self.prev_state[2]
+                #self.log("angle" + str(delta))
                 return delta >= degrees if degrees > 0 else delta <= degrees
             return did_turn_degrees
 
         def did_drive(distance_in_mm):
             def did_drive_distance():
-                delta = self.robot.distance() - self.prev_state.distance
+                delta = self.robot.distance() - self.prev_state[0]
                 return delta >= distance_in_mm
             return did_drive_distance
 
@@ -101,43 +106,52 @@ class Robot:
                 (self.check_blueline, NEXT_LEVEL_STATE),
                 (lambda: self.dist_sensor.distance() < OBSTACLE_DISTANCE, OBSTACLE_STATE),
             ]
-            return s(new_transitions + transitions, on_enter)
+            return State(new_transitions + transitions, on_enter)
 
         def drive_regulated():
             brightness = self.color_sensor.reflection()
             delta = brightness - mid
             turn_rate = K * delta
-            self.robot.drive(self.DRIVE_SPEED, turn_rate)
-            wait(10)
+            self.robot.drive(DRIVE_SPEED, turn_rate)
+            wait(WAIT_TIME)
 
         def drive_straight():
-            self.robot.drive(self.DRIVE_SPEED, 0)
+            self.robot.drive(DRIVE_SPEED, 0)
+            wait(WAIT_TIME)
 
         states = {
             "start": check_events(
                 [(is_gray, "drive_regulated"), (is_dark, "store_and_turn_right"), (is_light, "turn_left")],
-                lambda: wait(10),
+                lambda: wait(WAIT_TIME),
             ),
-            "turn_left": s([(is_gray, "drive_regulated")], turn(15)),
+            "turn_left": State([(is_gray, "drive_regulated")], turn(TURN_RATE)),
             "drive_regulated": check_events(
-                [(is_dark, "store_and_drive_right"), (True, "turn_left")], drive_regulated
+                [(is_dark, "store_and_turn_right"), (is_light, "turn_left")], drive_regulated
             ),
-            "store_and_turn_right": s([(True, "turn_right")], store_state),
-            "turn_right": s(
-                [(is_gray, "drive_regulated"), (did_turn(GAP_THRESHOLD_ANGLE), "store_and_turn_back_left")],
-                turn(-15),
+            "store_and_turn_right": State([(True, "turn_right")], store_state),
+            "turn_right": State(
+                [(is_gray, "drive_regulated"), (did_turn(-GAP_THRESHOLD_ANGLE), "store_and_turn_back_left")],
+                turn(-TURN_RATE),
             ),
-            "store_and_turn_back_left": s([(True, "turn_back_left")], store_state),
-            "turn_back_left": s([(did_turn(GAP_TURNBACK_ANGLE), "store_and_drive_straight")], turn(15)),
+            "store_and_turn_back_left": State([(True, "turn_back_left")], store_state),
+            "turn_back_left": State([(did_turn(GAP_TURNBACK_ANGLE), "store_and_drive_straight")], turn(TURNBACK_RATE)),
             "store_and_drive_straight": check_events([(True, "drive_straight")], store_state),
             "drive_straight": check_events([(did_drive(100), "start")], drive_straight),
+            "obst_1": State([(did_turn(GAP_THRESHOLD_ANGLE), "obst_2")], turn(TURNBACK_RATE)),
+            "obst_2": State([(did_drive(200), "obst_3")], drive_straight),
+            "obst_3": State([(did_turn(-GAP_THRESHOLD_ANGLE), "obst_4")], turn(-TURNBACK_RATE)),
+            "obst_4": State([(did_drive(400), "obst_5")], drive_straight),
+            "obst_5": State([(did_turn(-GAP_THRESHOLD_ANGLE), "obst_6")], turn(-TURNBACK_RATE)),
+            "obst_6": State([(did_drive(200), "obst_7")], drive_straight),
+            "obst_7": State([(did_turn(GAP_THRESHOLD_ANGLE), "start")], turn(TURNBACK_RATE)),
+
         }
         cur_state = states["start"]
 
         while not self.course.should_abort():
             successor = cur_state.check_conditions()
-            if not successor:
-                cur_state = states[successor]
+            if successor:
+                cur_state = states.get(successor, successor)
                 if cur_state == NEXT_LEVEL_STATE:
                     self.robot.stop()
                     self.course.running = False
@@ -146,11 +160,13 @@ class Robot:
                 elif cur_state == OBSTACLE_STATE:
                     self.robot.stop()
                     self.log("Obstacle found, circumnavigating...")
-                    continue
+                    return
 
                 if cur_state.on_enter:
                     cur_state.on_enter()
-                print("set state to", successor)
+        self.robot.stop()
+            #print("set state to", successor)
+            #self.log(successor)
 
     # Stage 2: Push a block in the corner
     def obstacle(self):
